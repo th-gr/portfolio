765d1741b08c773d8afce7bf10b99ac98ea1aad0
ThomasGrare <thomasgrare64@gmail.com>
2024-01-16 22:59:34 +0100

    animation rotation logo

--- src/components/Logo/ThomasLogo.vue
+++ src/components/Logo/ThomasLogo.vue
@@ -1,24 +1,38 @@
 <template>
-  <TresCanvas v-bind="gl">
-    <TresPerspectiveCamera :position="[0.6, 0.2, 0.4]" :look-at="[0, 2, 0]" />
+  <TresCanvas v-bind="gl" class="canvas">
+  <TresPerspectiveCamera :fov="90"/>
     <OrbitControls />
-    <Suspense>
-      <LogoComponent></LogoComponent>
-    </Suspense>
+    <TresMesh ref="boxRef" :position="[0, 0, 0]">
+      <Suspense>
+        <LogoComponent></LogoComponent>
+      </Suspense>
+      <TresMeshNormalMaterial />
+    </TresMesh>
     <TresDirectionalLight :position="[3, 3, 3]" :intensity="1" />
     <TresAmbientLight :intensity="2" />
   </TresCanvas>
 </template>
   
 <script setup>
-import { TresCanvas } from "@tresjs/core";
+import { shallowRef } from "vue";
+import { TresCanvas, useRenderLoop } from "@tresjs/core";
 import { OrbitControls } from "@tresjs/cientos";
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from "three";
 import LogoComponent from "./LogoComponent.vue";
-// import { onMounted } from "vue";
+
+const boxRef = shallowRef(null);
+const { onLoop } = useRenderLoop();
+
+
+onLoop(({ delta, elapsed }) => {
+  if (boxRef.value) {
+    boxRef.value.rotation.y += delta;
+    boxRef.value.rotation.z = elapsed * 0.2;
+  }
+});
 
 const gl = {
-  clearColor: "#F78B3D",
+  clearColor: "#756596",
   shadows: true,
   alpha: false,
   shadowMapType: BasicShadowMap,
